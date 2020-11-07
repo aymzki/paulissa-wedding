44fc1c72d0a72d63790e6a706a4be804fe408623
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2020-11-07 15:52:54 -0500

    our story section

--- src/components/Ourstory.js
+++ src/components/Ourstory.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Infographic from '../images/infographic.png';
 import './Ourstory.css';
 
 
 function Ourstory() {
     return (
         <div className='ourstory-container'>
-        
+        <img src={Infographic} alt='infographic' class="ourinfographic"></img>
         </div>
     )
 }
