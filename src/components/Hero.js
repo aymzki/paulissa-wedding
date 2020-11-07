e3712ca315bc4e394ad7301ee10d4183b25c27c7
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2020-11-06 22:51:42 -0500

    hero container

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+
+
+function Hero() {
+    return (
+        <div className='hero-container'>
+        
+        </div>
+    )
+}
+
+export default Hero
