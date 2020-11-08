474780b92e0d1fbf76b0fb35d6760c694684582e
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2020-11-08 12:37:55 -0500

    routing for home and tweaking nav mobile

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Hero}/>
+          <Route path="/home" exact component={Hero}/>
           <Route path="/ourstory" exact component={Ourstory}/>
           <Route path="/wedding" exact component={Wedding}/>
         </Switch>
