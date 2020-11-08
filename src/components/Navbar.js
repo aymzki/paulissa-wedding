2781f5c0d53f5a269b51e9f051d00039643a5d0c
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2020-11-08 15:56:16 -0500

    adding process env to navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,14 +13,14 @@ function Navbar() {
             <>
         <nav className='navbar'>
             <div className='navbar-continer'>
-               <Link to="/" className='navbar-logo'>
+               <Link to={process.env.PUBLIC_URL + '/'} className='navbar-logo'>
                    Paul & Alissa </Link> 
                 <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}> 
                 <li className='nav-item'>
-                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                <Link to={process.env.PUBLIC_URL + '/'} className='nav-links' onClick={closeMobileMenu}>
                     Home
                 </Link>
                 </li>
